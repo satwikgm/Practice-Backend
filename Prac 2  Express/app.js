010a0e7c8ff2e18c86f4ddfb3e00002bd6690ae0
unknown <satwikmishra06092001@gmail.com>
2023-03-22 19:28:37 +0530

    Third Commit

---
+++ Prac 2  Express/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+
+// Send HTML files
+app.get("/" , (req,res)=>{
+    res.sendFile('./views/index.html' ,  {root:__dirname})
+})
+
+app.get("/about" , (req,res)=>{
+    res.sendFile('./views/about.html' ,  {root:__dirname})
+})
+
+// Redirects
+app.get('/about-us' , (req,res)=>{
+    res.redirect('/about');
+});
+
+// 404 page
+// app.use is executed when no app.get above gets executed , its analogous to default in switch
+app.use((req,res)=>{
+    // res.statusCode = 404;
+    res.status(404).sendFile('./views/404.html' , {root:__dirname});
+})
+
+app.listen(3000 , ()=>{
+    console.log("Server started on port 3000");
+})
