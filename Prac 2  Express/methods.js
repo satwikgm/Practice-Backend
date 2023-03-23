27576edf08818c34040a56b970508ed793462ba7
unknown <satwikmishra06092001@gmail.com>
2023-03-23 22:21:15 +0530

    fourth commit

--- Prac 2  Express/methods.js
+++ Prac 2  Express/methods.js
@@ -2,54 +2,55 @@ const express = require('express');
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 
-
 app.listen(3000 , ()=>{
-    console.log("Server started on 3000");
+    console.log("Server started on port 3000");
 });
 
-// Middle ware function => Used in Post requests , Data from frontend converted to json 
+let users = {}
 
-let users = {};
-
-
-// get request
-app.get('/users' , (req,res) => {
+// get : Used to retrieve data from a server , 
+// Here we are displaying the users object that we will receive from server
+app.get("/users" , (req,res)=>{
     res.send(users);
-})  
+});
 
-// post request : To send data from frontend to backend
+// post : Used to send(submit) data from frontend to backend , to submit 
 app.post('/users' , (req,res)=>{
+    // received something from the frontend
     console.log(req.body);
     users = req.body;
-    // Same as res.send
+
+    // We are sending response as a json object to front end.
+    // There are two keys in our response message and user.
     res.json({
-        message:"Data received successfully",
+        message : "Data received successfully" , 
+        // Send this user response as frontend body in the postman body
+        // user is an object like users that will be displayed as frontend
         user : req.body
-    });
-});
+    })
+})
 
-// update request : req obtained here is to update something
-app.patch('/users',(req,res)=>{
-    // Print data from req body
+// patch : Used to update a specific resource
+app.patch('/users' , (req,res)=>{
+    // Request of update
     console.log('req-body-data-> ' , req.body);
-    // update data in users object
-
+    // This data has to be updated in the users object
     let dataToBeUpdated = req.body;
     for(key in dataToBeUpdated) {
         users[key] = dataToBeUpdated[key];
     }
     res.json({
-        message : "Data updated successfully" 
+        message : "Data updated successfully"
     })
 })
 
-
-// delete request : To delete data
+// delete : Used to delete specific resource
 app.delete('/users' , (req,res)=>{
-    users = {};
+    users = {}
     res.json({
-        message : "Data has been deleted"
-    })
-})
+        message : "Data has been successfully deleted"
+    });
+});
