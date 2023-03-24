39e7d12e1349ff6881ece46c764a5aad48d371e6
unknown <satwikmishra06092001@gmail.com>
2023-03-24 16:45:01 +0530

    Seventh Commit

---
+++ Prac 5 Express Mounting/methods.js
@@ -0,0 +1,86 @@
+const express = require('express');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.listen(3000 , ()=>{
+    console.log("Server started on port 3000");
+});
+
+let users = [];
+
+// mini app
+const userRouter = express.Router();
+// base route
+app.use('/users',userRouter);   // Base Router is /users
+
+// For get , post , patch , delete and queries
+userRouter
+    .route("/")                 // First parameter in any normal route
+    .get(getUsers)              // Second parameters (Functions) ....
+    .post(postUsers)
+    .patch(updateUsers)
+    .delete(deleteUsers);
+
+// For params 
+userRouter.route("/:id").get(getUsersById);
+
+
+
+// Instead of using these we are using the above router to route users
+// // get : Used to retrieve data from a server , 
+// app.get("/users" , );
+
+// // post : Used to send(submit) data from frontend to backend , to submit 
+// app.post('/users' , )
+
+// // patch : Used to update a specific resource
+// app.patch('/users' , )
+
+// // delete : Used to delete specific resource
+// app.delete('/users' , );
+
+// // params : Included in url of the http requests , Used to pass additional information 
+// app.get('/users/:id' , (req,res)=>{
+// })
+
+function getUsers(req,res) {
+    console.log(req.query);
+    res.send(users);
+}
+
+function postUsers(req,res) {
+    console.log(req.body);
+    users = req.body;
+
+    res.json({
+        message : "Data received successfully" , 
+        user : req.body
+    })
+}
+
+function updateUsers(req,res) {
+    console.log('req-body-data-> ' , req.body);
+    let dataToBeUpdated = req.body;
+    for(key in dataToBeUpdated) {
+        users[key] = dataToBeUpdated[key];
+    }
+    res.json({
+        message : "Data updated successfully"
+    })
+}
+
+function deleteUsers(req,res) {
+    users = {}
+    res.json({
+        message : "Data has been successfully deleted"
+    });
+}
+
+function getUsersById(req,res) {
+    console.log(req.params.id);
+    console.log(req.params);
+    res.send("User id created")
+}
