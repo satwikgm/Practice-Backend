eb87dc17a83d66e44d3822839a643cc29bdae745
unknown <satwikmishra06092001@gmail.com>
2023-03-25 10:32:40 +0530

    Ninth Commit Middleware

--- Prac 6 Signup Page Axios/app.js
+++ Prac 6 Signup Page Axios/app.js
@@ -9,36 +9,48 @@ app.listen(3000 , ()=>{
     console.log("Server started on port 3000");
 });
 
-const users = [
-    { name: 'John', id: 1 },
-    { name: 'Jane', id: 2 },
-    { name: 'Bob', id: 3 },
-    { name: 'Alice', id: 4 },
-    { name : 'John2' , id : 1}
-];
+// const users = [
+//     { name: 'John', id: 1 },
+//     { name: 'Jane', id: 2 },
+//     { name: 'Bob', id: 3 },
+//     { name: 'Alice', id: 4 },
+//     { name : 'John2' , id : 1}
+// ];
   
 // mini app
-const userRouter = express.Router();
+// const userRouter = express.Router();
 const authRouter = express.Router();
 // base route
-app.use('/users',userRouter);   // Base Router is /users
+// app.use('/users',userRouter);   // Base Router is /users
 app.use('/auth',authRouter);   // Base Router is /auth
 
 // For get , post , patch , delete and queries
-userRouter
-    .route("/")                 // First parameter in any normal route
-    .get(getUsers)              // Second parameters (Functions) ....
-    .post(postUsers)
-    .patch(updateUsers)
-    .delete(deleteUsers);
+// userRouter
+//     .route("/")                 // First parameter in any normal route
+//     .get(getUsers)              // Second parameters (Functions) ....
+//     .post(postUsers)
+//     .patch(updateUsers)
+//     .delete(deleteUsers);
 
 // For params 
-userRouter.route("/:id").get(getUsersById);
+// userRouter.route("/:id").get(getUsersById);
 
+// authRouter
+//     .route('/signup')
+//     .get(getSignUp)
+//     .post(postSignUp);
+
+// middleware
 authRouter
     .route('/signup')
-    .get(getSignUp)
+    .get(middleware , getSignUp)
     .post(postSignUp);
+
+function middleware(req,res,next) {
+    console.log('Middleware encountered');
+    next();
+}
+
 
 // Instead of using these we are using the above router to route users
 // // get : Used to retrieve data from a server , 
@@ -57,56 +69,57 @@ authRouter
 // app.get('/users/:id' , (req,res)=>{
 // })
 
-function getUsers(req,res) {
-    console.log(req.query);
-    res.send(users);
-}
+// function getUsers(req,res) {
+//     console.log(req.query);
+//     res.send(users);
+// }
 
-function postUsers(req,res) {
-    console.log(req.body);
-    users = req.body;
+// function postUsers(req,res) {
+//     console.log(req.body);
+//     users = req.body;
 
-    res.json({
-        message : "Data received successfully" , 
-        user : req.body
-    })
-}
+//     res.json({
+//         message : "Data received successfully" , 
+//         user : req.body
+//     })
+// }
 
-function updateUsers(req,res) {
-    console.log('req-body-data-> ' , req.body);
-    let dataToBeUpdated = req.body;
-    for(key in dataToBeUpdated) {
-        users[key] = dataToBeUpdated[key];
-    }
-    res.json({
-        message : "Data updated successfully"
-    })
-}
+// function updateUsers(req,res) {
+//     console.log('req-body-data-> ' , req.body);
+//     let dataToBeUpdated = req.body;
+//     for(key in dataToBeUpdated) {
+//         users[key] = dataToBeUpdated[key];
+//     }
+//     res.json({
+//         message : "Data updated successfully"
+//     })
+// }
 
-function deleteUsers(req,res) {
-    users = {}
-    res.json({
-        message : "Data has been successfully deleted"
-    });
-}
+// function deleteUsers(req,res) {
+//     users = {}
+//     res.json({
+//         message : "Data has been successfully deleted"
+//     });
+// }
 
-function getUsersById(req,res) {
-    console.log(req.params.id);
-    let paramsId = req.params.id;
-    let obj = {};
-    for(let i=0;i<users.length;i++) {
-        if(users[i]['id'] == paramsId) {
-            obj = users[i];
-        }
-    }
-    res.json({
-        message : "request received",
-        data : obj
-    });
-}
+// function getUsersById(req,res) {
+//     console.log(req.params.id);
+//     let paramsId = req.params.id;
+//     let obj = {};
+//     for(let i=0;i<users.length;i++) {
+//         if(users[i]['id'] == paramsId) {
+//             obj = users[i];
+//         }
+//     }
+//     res.json({
+//         message : "request received",
+//         data : obj
+//     });
+// }
 
 // Send HTML files
 function getSignUp(req,res) {
+    console.log('Now get signup is called');
     res.sendFile('/public/index.html' , {root : __dirname});
 }
 
